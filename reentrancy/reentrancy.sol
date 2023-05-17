ebb5aa440bc725cb799a40bd1c496411318ad10b
Absolute <106808580+chiemezie1@users.noreply.github.com>
2023-05-17 19:20:34 +0100

    Example of reentrancy

--- reentrancy/reentrancy.sol
+++ reentrancy/reentrancy.sol
@@ -1,1 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
+contract ContractA {
+    uint public balance;
+
+    function vulnerableFunction(address payable recipient) external {
+        // Update balance
+        balance += 10;
+
+        // Call external contract
+        (bool success, ) = recipient.call{value: 10 ether}("");
+        require(success, "External call failed");
+
+        // Deduct balance
+        if (balance >= 10) {
+            balance -= 10;
+        }
+    }
+}
+
+contract ContractB {
+    ContractA public contractA;
+
+    constructor(address contractAAddress) {
+        contractA = ContractA(contractAAddress);
+    }
+
+    function attack() external payable {
+        // Call vulnerable function repeatedly (fallback)
+        contractA.vulnerableFunction(payable(address(this)));
+    }
+
+    function getContractABalance() public view returns (uint) {
+        return contractA.balance();
+    }
+}
