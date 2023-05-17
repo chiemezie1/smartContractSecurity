02fe460f95d604abe1a21014621f936dabedf7d4
Absolute <106808580+chiemezie1@users.noreply.github.com>
2023-05-17 19:55:23 +0100

    Update reentrancyContractB.sol

--- reentrancy/reentrancyContractB.sol
+++ reentrancy/reentrancyContractB.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
+import "./reentrancyContractA.sol.sol";
+
+contract ContractB {
+    ContractA public contractA;
+
+    constructor(address contractAAddress) {
+        contractA = ContractA(contractAAddress);
+    }
+
+    function attack() external payable {
+        // Call vulnerable function repeatedly (fallback)
+        contractA.vulnerableFunction(payable(address(this)));
+    }
+
+    function getContractABalance() public view returns (uint) {
+        return contractA.balance();
+    }
+}
