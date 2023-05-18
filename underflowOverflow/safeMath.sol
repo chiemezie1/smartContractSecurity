1b3bda9a6193406372e332edf55f693d61d815e4
Absolute <106808580+chiemezie1@users.noreply.github.com>
2023-05-18 20:30:07 +0100

    Update safeMath.sol

--- underflowOverflow/safeMath.sol
+++ underflowOverflow/safeMath.sol
@@ -1,1 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
+contract SafeMath {
+
+    function safeMul(uint a, uint b) internal pure returns (uint) {
+        uint c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function safeDiv(uint a, uint b) internal pure returns
+        (uint) {  //inline assembly block in Solidity
+        assert(b > 0);
+        uint c = a / b;
+        assert(a == b * c + a % b);
+        return c;
+    }
+
+    function safeSub(uint a, uint b) internal pure returns
+        (uint) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function safeAdd(uint a, uint b) internal pure returns
+        (uint) {
+        uint c = a + b;
+        assert(c>=a && c>=b);
+        return c;
+    }
+}
