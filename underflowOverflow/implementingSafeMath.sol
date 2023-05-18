704c2e37455fbd915c3bebff3cf363ad41f2eb53
Absolute <106808580+chiemezie1@users.noreply.github.com>
2023-05-18 22:19:11 +0100

    Update implementingSafeMath.sol

--- underflowOverflow/implementingSafeMath.sol
+++ underflowOverflow/implementingSafeMath.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
+import "./SafeMath.sol";
+
+contract implementingSafe {
+
+    using SafeMath for uint256;
+
+    uint256 public total;
+
+    function add(uint256 a, uint256 b) public returns(uint) {
+       return(total = a.add(b));
+    }
+
+    function subtract(uint256 a, uint256 b) public returns(uint)  {
+      return  total = a.sub(b);
+    }
+
+    function multiply(uint256 a, uint256 b) public returns(uint)  {
+        return total = a.mul(b);
+    }
+
+    function divide(uint256 a, uint256 b) public returns(uint)  {
+        return total = a.div(b);
+    }
+}
